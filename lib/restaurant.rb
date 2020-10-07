e0ca780de8f658be0e35ef0c1ebb7ba90552e80d
Jordan Beck <jordanfbeck@gmail.com>
2020-10-07 13:54:01 -0600

    Add restaurant.rb

---
+++ lib/restaurant.rb
@@ -0,0 +1,20 @@
+
+
+class Restaurant
+
+    attr_reader :opening_time, :name
+  def initialize(opening_time, name)
+    @opening_time = opening_time
+    @name = name
+    @dishes = []
+
+  end
+
+  def dishes
+    @dishes
+  end
+
+  def closing_time(time)
+    @opening_time += time.to_s
+  end
+end
--- test/restaurant_test.rb
+++ test/restaurant_test.rb
@@ -11,21 +11,21 @@ class RestaurantTest < Minitest::Test
   end
 
   def test_it_has_opening_time
-    skip
+    # skip
     restaurant = Restaurant.new('10:00', 'Fuel Cafe')
 
     assert_equal '10:00', restaurant.opening_time
   end
 
   def test_it_has_name
-    skip
+    # skip
     restaurant = Restaurant.new('10:00', 'Fuel Cafe')
 
     assert_equal 'Fuel Cafe', restaurant.name
   end
 
   def test_it_has_dishes
-    skip
+    # skip
     restaurant = Restaurant.new('10:00', 'Fuel Cafe')
 
     assert_equal [], restaurant.dishes
@@ -33,7 +33,7 @@ class RestaurantTest < Minitest::Test
 
   #Iteration 2 Tests:
   def test_it_has_closing_time
-    skip
+    # skip
     restaurant1 = Restaurant.new('10:00', 'Fuel Cafe')
     restaurant2 = Restaurant.new('16:00', 'Il Poggio')
 
